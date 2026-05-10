6d8679c2588b100bcb9e34087c97429cc421e76b
dianerdiana <dianerdiana.de@gmail.com>
2026-05-10 17:10:16 +0700

    feat: create AuthUser type and update Express Request interface to use AuthUser

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,14 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as express from 'express';
+
 import { UserRole } from '../common/enums/user-role.enum';
+
+import { AuthUser } from './auth-user.type';
 
 declare global {
   namespace Express {
     interface Request {
-      user?: {
-        id: string;
-        role: UserRole;
-      };
+      user?: AuthUser;
     }
   }
 }
-
-export {};
