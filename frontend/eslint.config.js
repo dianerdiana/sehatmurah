272b9cd334d148e8018202ec65d98f61788054b9
dianerdiana <dianerdiana.de@gmail.com>
2026-05-14 20:20:47 +0700

    feat: enhance ESLint configuration with simple-import-sort for improved import organization

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,14 +1,58 @@
 //  @ts-check
 
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import { tanstackConfig } from '@tanstack/eslint-config'
 
 export default [
   ...tanstackConfig,
   {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+  },
+  {
     rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Node builtin
+            ['^node:'],
+
+            // React
+            ['^react'],
+
+            // Vite
+            ['^vite'],
+
+            // External packages
+            ['^@?\\w'],
+
+            // Internal alias
+            ['^@/components'],
+            ['^@/hooks'],
+            ['^@/integrations'],
+            ['^@/lib'],
+            ['^@/routes'],
+            ['^@/services'],
+            ['^@/utils'],
+
+            // Parent
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+
+            // Same folder
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+
+            // Style
+            ['^.+\\.?(css|scss)$'],
+          ],
+        },
+      ],
+
+      'simple-import-sort/exports': 'error',
       'import/no-cycle': 'off',
-      'import/order': 'on',
-      'sort-imports': 'on',
+      'import/order': 'off',
+      'sort-imports': 'off',
       '@typescript-eslint/array-type': 'off',
       '@typescript-eslint/require-await': 'off',
       'pnpm/json-enforce-catalog': 'off',
