b0cb7af19d7a4a5e2d849677fa74533410429894
dianerdiana <dianerdiana.de@gmail.com>
2026-05-15 13:31:35 +0700

    feat: add form context and custom hook for form handling

---
+++ frontend/src/hooks/use-app-form.ts
@@ -0,0 +1,14 @@
+import { createFormHook } from '@tanstack/react-form'
+
+import { TextField } from '@/components/forms/text-field'
+
+import { fieldContext, formContext } from '@/context/form-context'
+
+export const { useAppForm } = createFormHook({
+  fieldComponents: {
+    TextField,
+  },
+  formComponents: {},
+  fieldContext,
+  formContext,
+})
