0999cbe5a100eb3aabe6dddb262f56aabdbcd060
dianerdiana <dianerdiana.de@gmail.com>
2026-05-17 13:07:40 +0700

    feat: add utility functions for class name merging and ability creation

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,7 +1,15 @@
+import type { AbilityRule } from '@/types/ability-rule';
+import { createMongoAbility } from '@casl/ability';
 import type { ClassValue } from 'clsx';
 import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function createAbility(rules: AbilityRule[]) {
+  return createMongoAbility(rules, {
+    detectSubjectType: (object: any) => object!.type,
+  });
 }
