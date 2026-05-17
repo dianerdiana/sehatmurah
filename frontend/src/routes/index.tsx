35a6535dc39bd910e95c8dc96ae05ec5bc9c5dc9
dianerdiana <dianerdiana.de@gmail.com>
2026-05-17 17:29:21 +0700

    feat: replace GridSpecialists component with SpecialistCategories in HomePage

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router';
 
 import { PublicFacingLayout } from '@/layouts/public-facing-layout';
-import { GridSpecialists } from '@/modules/public-facing/components/grid-specialists';
 import { HomeHeader } from '@/modules/public-facing/components/home-header';
 import { RecommendedDoctors } from '@/modules/public-facing/components/recommended-doctors';
+import { SpecialistCategories } from '@/modules/public-facing/components/specialist-categories';
 
 export const Route = createFileRoute('/')({ component: HomePage });
 
@@ -11,7 +11,7 @@ function HomePage() {
   return (
     <PublicFacingLayout>
       <HomeHeader />
-      <GridSpecialists />
+      <SpecialistCategories />
       <RecommendedDoctors />
     </PublicFacingLayout>
   );
