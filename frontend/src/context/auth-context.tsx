046e4811b6f638a4466f9346b32ac09e75089e92
dianerdiana <dianerdiana.de@gmail.com>
2026-05-17 13:06:58 +0700

    feat: implement AuthContext for user authentication and registration

---
+++ frontend/src/context/auth-context.tsx
@@ -0,0 +1,91 @@
+import { createContext, useContext, useState } from 'react';
+
+import { api } from '@/configs/api-config';
+
+import type { AxiosResponse } from 'axios';
+
+import type { AbilityRule } from '@/types/ability-rule';
+import type { UserData } from '@/types/user-data';
+import type { LoginResponse } from '@/modules/auth/auth.response';
+
+import { AbilityContext } from './ability-context';
+
+import { createAbility } from '@/lib/utils';
+import type { HttpResponse } from '@/types/http-response';
+import { toApiError } from '@/configs/auth/jwt-service';
+import { UserRole } from '@/types/enums/user-role.enum';
+import type { LoginDto } from '@/modules/auth/auth.schema';
+
+type AuthContextType = {
+  isAuthenticated: boolean;
+  isInitialLoading: boolean;
+  login: (credentials: any) => Promise<HttpResponse<LoginResponse>>;
+  register: (credentials: any) => Promise<HttpResponse<LoginResponse>>;
+  userData: UserData;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [isInitialLoading, setIsInitialLoading] = useState(true);
+  const ability = useContext(AbilityContext);
+
+  const updateAbility = (permissions: AbilityRule[]) => {
+    const newAbility = createAbility(permissions);
+    ability.update(newAbility.rules);
+  };
+
+  const login = async (credentials: any): Promise<HttpResponse<LoginResponse>> => {
+    try {
+      const response = await api.post<LoginDto, HttpResponse<LoginResponse>>('/auth/login', credentials);
+
+      if (response.data.status === 'success') {
+        const { data } = response.data;
+        updateAbility([{ action: 'read', subject: 'Auth' }]);
+        api.setToken(data.token);
+        setUserData(data.user);
+        setIsInitialLoading(false);
+      }
+
+      return response.data;
+    } catch (error) {
+      return toApiError(error);
+    }
+  };
+
+  const register = async (credentials: any): Promise<AxiosResponse<HttpResponse<LoginResponse>> | any> => {
+    try {
+      const response = await api.register(credentials);
+
+      setIsInitialLoading(false);
+
+      return response.data;
+    } catch (error) {
+      throw toApiError(error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated: !!userData,
+        isInitialLoading,
+        login,
+        register,
+        userData: userData
+          ? userData
+          : ({
+              id: '',
+              role: UserRole.PATIENT,
+              name: '',
+              email: '',
+            } as UserData),
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthContextProvider };
