10dfb4fd0a57a1147ea4a28aa83f113dae34cecc
dianerdiana <dianerdiana.de@gmail.com>
2026-05-17 23:18:11 +0700

    fix: update Appointment link to point to the correct search route

--- frontend/src/layouts/public-facing-layout.tsx
+++ frontend/src/layouts/public-facing-layout.tsx
@@ -15,7 +15,7 @@ const navigation = [
     title: 'Appointment',
     activeIcon: 'assets/icons/homepage-nav-appointment-active.svg',
     nonActiveIcon: 'assets/icons/homepage-nav-appointment-nonactive.svg',
-    href: '/appointments',
+    href: '/search',
   },
   {
     id: 3,
@@ -28,28 +28,26 @@ const navigation = [
 
 export function PublicFacingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className='mx-auto max-w-160 overflow-hidden pb-24.5'>
-      <div className='bg-gray-50'>
-        {children}
-        <nav className='navigate fixed bottom-0 left-0 right-0 z-30 mx-auto'>
-          <div className='mx-auto max-w-160'>
-            <div className='flex items-center justify-between rounded-t-2xl border-t border-gray-50 bg-white px-4 py-6'>
-              {navigation.map((nav) => (
-                <Link to={nav.href} key={nav.id} className='grid w-[112.33px] place-items-center gap-1.5'>
-                  {({ isActive }) => (
-                    <>
-                      <img src={isActive ? nav.activeIcon : nav.nonActiveIcon} alt='Icon' />
-                      <p className={`font-bold leading-[20.16px] ${isActive ? 'text-primary' : 'text-gray-500'}`}>
-                        {nav.title}
-                      </p>
-                    </>
-                  )}
-                </Link>
-              ))}
-            </div>
+    <main className='mx-auto max-w-160 overflow-hidden pb-24.5 min-h-screen'>
+      {children}
+      <nav className='navigate fixed bottom-0 left-0 right-0 z-30 mx-auto'>
+        <div className='mx-auto max-w-160'>
+          <div className='flex items-center justify-between rounded-t-2xl border-t border-gray-50 bg-white px-4 py-6'>
+            {navigation.map((nav) => (
+              <Link to={nav.href} key={nav.id} className='grid w-[112.33px] place-items-center gap-1.5'>
+                {({ isActive }) => (
+                  <>
+                    <img src={isActive ? nav.activeIcon : nav.nonActiveIcon} alt='Icon' />
+                    <p className={`font-bold leading-[20.16px] ${isActive ? 'text-primary' : 'text-gray-500'}`}>
+                      {nav.title}
+                    </p>
+                  </>
+                )}
+              </Link>
+            ))}
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </main>
   );
 }
