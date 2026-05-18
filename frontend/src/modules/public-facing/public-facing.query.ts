5057d3d70cb13c1ba28a0601755fc5fe56ac72a8
dianerdiana <dianerdiana.de@gmail.com>
2026-05-18 13:39:56 +0700

    refactor: remove unused API response types and update query functions for specialists and doctors

--- frontend/src/modules/public-facing/public-facing.query.ts
+++ frontend/src/modules/public-facing/public-facing.query.ts
@@ -1,10 +1,39 @@
-import { queryOptions } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
-import { getDoctors } from './public-facing.api';
+import type { ErrorResponse } from '@/types/api-response.type';
+import type { Doctor } from '@/types/doctor.type';
+import type { Specialist } from '@/types/specialist.type';
 
-export function getDoctorsOption() {
-  return queryOptions({
-    queryKey: ['doctors'],
-    queryFn: () => getDoctors({}),
+export const useGetDoctors = ({
+  params,
+  enabled = true,
+}: {
+  params?: { city?: string; specialist?: string };
+  enabled?: boolean;
+}) => {
+  return useQuery<Doctor[], ErrorResponse>({
+    queryKey: ['/doctors', params],
+    enabled,
   });
-}
+};
+
+export const useGetDoctorsCity = ({ params }: { params?: { search?: string } }) => {
+  return useQuery<string[], ErrorResponse>({
+    queryKey: ['/doctors/cities', params],
+    placeholderData: [],
+  });
+};
+
+export const useGetSpecialists = ({
+  params,
+}: {
+  params?: {
+    limit?: number;
+    page?: number;
+  };
+}) => {
+  return useQuery<Specialist[], ErrorResponse>({
+    queryKey: ['/specialists', params],
+    placeholderData: [],
+  });
+};
