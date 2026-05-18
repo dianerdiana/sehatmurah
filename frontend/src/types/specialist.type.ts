5486b9a954f19b7f5b19f89efc050359c4660987
dianerdiana <dianerdiana.de@gmail.com>
2026-05-18 13:39:32 +0700

    feat: add Specialist type definition for improved type safety

---
+++ frontend/src/types/specialist.type.ts
@@ -0,0 +1,14 @@
+export type Specialist = {
+  _id: string;
+  name: string;
+  slug: string;
+  description: string;
+  icon: string;
+  image: string;
+  isActive: boolean;
+  sortOrder: number;
+  countDoctors: number;
+  __v: number;
+  createdAt: string;
+  updatedAt: string;
+};
