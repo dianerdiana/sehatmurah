7434ff79bb6761f94fecb0a18d695689aa7084fd
dianerdiana <dianerdiana.de@gmail.com>
2026-05-18 19:44:27 +0700

    fix: update layout max-width to 170 for consistency across components

--- frontend/src/layouts/public-facing-layout.tsx
+++ frontend/src/layouts/public-facing-layout.tsx
@@ -28,10 +28,10 @@ const navigation = [
 
 export function PublicFacingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className='mx-auto max-w-160 overflow-hidden pb-24.5 min-h-screen'>
+    <main className='mx-auto max-w-170 overflow-hidden pb-24.5 min-h-screen'>
       {children}
       <nav className='navigate fixed bottom-0 left-0 right-0 z-30 mx-auto'>
-        <div className='mx-auto max-w-160'>
+        <div className='mx-auto max-w-170'>
           <div className='flex items-center justify-between rounded-t-2xl border-t border-gray-50 bg-white px-4 py-6'>
             {navigation.map((nav) => (
               <Link to={nav.href} key={nav.id} className='grid w-[112.33px] place-items-center gap-1.5'>
