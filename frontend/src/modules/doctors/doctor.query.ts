c4de066d686ea6438badce18476aa7bb602c12ee
dianerdiana <dianerdiana.de@gmail.com>
2026-05-24 15:03:30 +0700

    feat: refactor doctor query options to use doctorKeys for queryKey management

--- frontend/src/modules/doctors/doctor.query.ts
+++ frontend/src/modules/doctors/doctor.query.ts
@@ -1,31 +1,32 @@
 import { queryOptions } from '@tanstack/react-query';
 
 import { doctorApi } from './doctor.api';
+import { doctorKeys } from './doctor.key';
 import type { ListDoctorsCitiesDto, ListDoctorsDto, ListReviewsByDoctorDto } from './doctor.schema';
 
 export const doctorQueryOptions = {
   list: (params?: ListDoctorsDto) =>
     queryOptions({
-      queryKey: ['/doctors', params],
+      queryKey: doctorKeys.list(params),
       queryFn: () => doctorApi.list(params),
     }),
 
   cities: (params?: ListDoctorsCitiesDto) =>
     queryOptions({
-      queryKey: ['/doctors/cities', params],
+      queryKey: doctorKeys.cities(params),
       queryFn: () => doctorApi.cities(params),
     }),
 
   getById: (id: string) =>
     queryOptions({
-      queryKey: ['/doctors', id],
+      queryKey: doctorKeys.detail(id),
       queryFn: () => doctorApi.getById(id),
       enabled: !!id,
     }),
 
   getDoctorReviews: (id: string, params?: ListReviewsByDoctorDto) =>
     queryOptions({
-      queryKey: ['/doctors', id, 'reviews', params],
+      queryKey: doctorKeys.reviews(id, params),
       queryFn: () => doctorApi.getDoctorReviews(id, params),
       enabled: !!id,
     }),
