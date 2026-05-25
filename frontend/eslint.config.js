bb233a5e84cc96d1e27592c0f38aba6d8113304b
dianerdiana <dianerdiana.de@gmail.com>
2026-05-25 17:50:45 +0700

    feat: disable react-refresh rule for component exports in ESLint configuration

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -90,6 +90,7 @@ export default defineConfig([
 
       'react-hooks/incompatible-library': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      'react-refresh/only-export-components': 'off',
     },
   },
   {
