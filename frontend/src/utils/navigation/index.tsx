f7b1ebe41e3fa5d1b45326e76fd60a25c56c521e
dianerdiana <dianerdiana.de@gmail.com>
2026-05-26 21:50:46 +0700

    feat: enhance navigation structure with type definitions and permissions for improved access control

--- frontend/src/utils/navigation/index.tsx
+++ frontend/src/utils/navigation/index.tsx
@@ -1,6 +1,26 @@
+import type React from 'react';
+
 import { Archive, BriefcaseBusiness, Clipboard, HeartPulse, Home, UserRoundCog, Users } from 'lucide-react';
 
-export const navigation = [
+import type { AbilityRule } from '@/types/ability-rule.type';
+
+export type NavigationGroup = {
+  label: string;
+  isActive?: boolean;
+  items: NavigationItem[];
+  permission?: AbilityRule;
+};
+
+export type NavigationItem = {
+  title: string;
+  url: string;
+  icon: React.ReactNode;
+  isActive?: boolean;
+  permission?: AbilityRule;
+  items?: NavigationItem[];
+};
+
+export const navigation: NavigationGroup[] = [
   {
     label: 'DASHBOARD',
     isActive: true,
@@ -9,6 +29,10 @@ export const navigation = [
         title: 'Dashboard',
         url: '/dashboard',
         icon: <Home />,
+        permission: {
+          action: 'read',
+          subject: 'Dashboard',
+        },
       },
     ],
   },
@@ -19,21 +43,37 @@ export const navigation = [
         title: 'Appointment',
         url: '/app/appointments',
         icon: <Clipboard />,
+        permission: {
+          action: 'read',
+          subject: 'ListAppointment',
+        },
       },
       {
         title: 'Doctor',
         url: '/app/doctors',
         icon: <Archive />,
+        permission: {
+          action: 'read',
+          subject: 'ListDoctor',
+        },
       },
       {
         title: 'Patient',
         url: '/app/patients',
         icon: <Users />,
+        permission: {
+          action: 'read',
+          subject: 'ListPatient',
+        },
       },
       {
         title: 'Specialist',
         url: '/app/specialists',
         icon: <HeartPulse />,
+        permission: {
+          action: 'read',
+          subject: 'ListSpecialist',
+        },
       },
     ],
   },
