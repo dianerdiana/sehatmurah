898c1f7dc0c4bdf0cfa3593a6ee18fb34dcee12d
dianerdiana <dianerdiana.de@gmail.com>
2026-05-26 21:50:15 +0700

    feat: expand PermissionSubject type to include additional subjects for enhanced access control

--- frontend/src/types/ability-rule.type.ts
+++ frontend/src/types/ability-rule.type.ts
@@ -1,7 +1,22 @@
+import type { MongoAbility, MongoQuery } from '@casl/ability';
+
 export type PermissionAction = 'create' | 'read' | 'update' | 'delete';
-export type PermissionSubject = 'Auth' | 'Appointment' | 'DoctorProfile' | 'PatientProfile' | 'Review' | 'Specialist';
+export type PermissionSubject =
+  | 'Auth'
+  | 'Appointment'
+  | 'DoctorProfile'
+  | 'PatientProfile'
+  | 'Review'
+  | 'Specialist'
+  | 'Dashboard'
+  | 'ListAppointment'
+  | 'ListDoctor'
+  | 'ListPatient'
+  | 'ListSpecialist';
 
 export type AbilityRule = {
   action: PermissionAction;
   subject: PermissionSubject;
 };
+
+export type AppAbility = MongoAbility<[PermissionAction, PermissionSubject], MongoQuery>;
