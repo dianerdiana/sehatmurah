1c71b42028a55b12f37a2e0a17f3727cee58d59b
dianerdiana <dianerdiana.de@gmail.com>
2026-05-26 21:50:22 +0700

    feat: update AbilityContext to use AppAbility type for improved type safety

--- frontend/src/utils/context/ability-context.tsx
+++ frontend/src/utils/context/ability-context.tsx
@@ -1,13 +1,10 @@
 import { createContext } from 'react';
 
-import type { MongoAbility } from '@casl/ability';
 import { Can as CaslCan, type CanProps } from '@casl/react';
 
 import { ability } from '@/configs/acl/initial-ability';
 
-import type { PermissionAction, PermissionSubject } from '@/types/ability-rule.type';
+import type { AppAbility } from '@/types/ability-rule.type';
 
-export type AppAbility = MongoAbility<[PermissionAction, PermissionSubject]>;
-
-export const AbilityContext = createContext(ability);
+export const AbilityContext = createContext<AppAbility>(ability);
 export const Can = (props: CanProps<AppAbility>) => <CaslCan {...props} />;
