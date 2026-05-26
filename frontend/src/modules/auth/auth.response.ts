4b9e4033839862b35997884a7d7c1a224186a183
dianerdiana <dianerdiana.de@gmail.com>
2026-05-26 22:22:24 +0700

    feat: update LoginResponse type to include full UserData and remove permissions field

--- frontend/src/modules/auth/auth.response.ts
+++ frontend/src/modules/auth/auth.response.ts
@@ -1,8 +1,6 @@
 import type { UserData } from '@/types/user-data.type';
-import type { AbilityRule } from '@/types/ability-rule.type';
 
 export type LoginResponse = {
   token: string;
-  user: Omit<UserData, 'permissions'>;
-  permissions: AbilityRule[];
+  user: UserData;
 };
