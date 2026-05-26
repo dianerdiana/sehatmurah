8646b69faaa168b19f2fe00c8fd7eb3c4a024005
dianerdiana <dianerdiana.de@gmail.com>
2026-05-26 13:44:50 +0700

    feat: remove redundant formatZodError function and clean up error handling in validateRequest middleware

--- backend/src/middlewares/validate-request.middleware.ts
+++ backend/src/middlewares/validate-request.middleware.ts
@@ -1,20 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import z, { ZodError } from 'zod';
 
-import { ApiError, ApiErrorItem } from '../common/api-error';
+import { ApiError } from '../common/api-error';
+import { formatZodError } from '../common/format-zod-error';
+import { deleteUploadFile } from '../utils/delete-upload-file';
 
 interface ValidationSchemas {
   body?: z.ZodObject;
   params?: z.ZodObject;
   query?: z.ZodObject;
 }
-
-const formatZodError = (error: ZodError): ApiErrorItem[] => {
-  return error.issues.map((issue) => ({
-    field: issue.path.length ? issue.path.join('.') : undefined,
-    message: issue.message,
-  }));
-};
 
 export const validateRequest = (schemas: ValidationSchemas) => {
   return (req: Request, _res: Response, next: NextFunction): void => {
@@ -42,6 +37,9 @@ export const validateRequest = (schemas: ValidationSchemas) => {
         );
         return;
       }
+
+      if (req.body.icon) deleteUploadFile(req.body.icon);
+      if (req.body.image) deleteUploadFile(req.body.image);
 
       next(error);
     }
