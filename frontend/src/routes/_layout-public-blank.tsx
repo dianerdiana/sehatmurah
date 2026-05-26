6e4889aee28cc563e82f975e621189fe1637242e
dianerdiana <dianerdiana.de@gmail.com>
2026-05-26 13:58:45 +0700

    feat: unify theme setting in PublicBlankLayout and PublicNavLayout components

--- frontend/src/routes/_layout-public-blank.tsx
+++ frontend/src/routes/_layout-public-blank.tsx
@@ -1,10 +1,16 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router';
 
+import { useTheme } from '@/utils/hooks/use-theme';
+
 export const Route = createFileRoute('/_layout-public-blank')({
-  component: PublicFacingBlankLayout,
+  component: PublicBlankLayout,
 });
 
-function PublicFacingBlankLayout() {
+function PublicBlankLayout() {
+  const { setTheme } = useTheme();
+
+  setTheme('light');
+
   return (
     <main className='mx-auto max-w-170 overflow-hidden pb-24.5 min-h-screen'>
       <Outlet />
--- frontend/src/routes/_layout-public-nav.tsx
+++ frontend/src/routes/_layout-public-nav.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router';
 import { Link } from '@tanstack/react-router';
 
+import { useTheme } from '@/utils/hooks/use-theme';
+
 export const Route = createFileRoute('/_layout-public-nav')({
   component: PublicNavLayout,
 });
@@ -30,6 +32,10 @@ const navigation = [
 ];
 
 function PublicNavLayout() {
+  const { setTheme } = useTheme();
+
+  setTheme('light');
+
   return (
     <main className='mx-auto max-w-170 overflow-hidden pb-24.5 min-h-screen'>
       <Outlet />
