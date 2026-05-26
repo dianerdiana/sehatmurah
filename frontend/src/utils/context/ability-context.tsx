2f4afbaf7b12793890e99972e40266c2a4658748
dianerdiana <dianerdiana.de@gmail.com>
2026-05-26 20:15:19 +0700

    feat: update AbilityContext to use typed AppAbility and enhance Can component

--- frontend/src/utils/context/ability-context.tsx
+++ frontend/src/utils/context/ability-context.tsx
@@ -1,5 +1,13 @@
 import { createContext } from 'react';
 
-import ability from '@/configs/acl/initial-ability';
+import type { MongoAbility } from '@casl/ability';
+import { Can as CaslCan, type CanProps } from '@casl/react';
+
+import { ability } from '@/configs/acl/initial-ability';
+
+import type { PermissionAction, PermissionSubject } from '@/types/ability-rule.type';
+
+export type AppAbility = MongoAbility<[PermissionAction, PermissionSubject]>;
 
 export const AbilityContext = createContext(ability);
+export const Can = (props: CanProps<AppAbility>) => <CaslCan {...props} />;
