bb3f01c87f43e5aa1728b8c6ab8cdb3989aea997
dianerdiana <dianerdiana.de@gmail.com>
2026-05-27 13:41:07 +0700

    feat: add authentication route with redirect logic

---
+++ frontend/src/routes/_layout-blank/auth.tsx
@@ -0,0 +1,19 @@
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
+import z from 'zod';
+
+import { getSafeRedirectTarget } from '@/utils/auth/route-guard';
+
+export const Route = createFileRoute('/_layout-blank/auth')({
+  validateSearch: z.object({
+    redirect: z.string().optional(),
+  }),
+  component: Outlet,
+  beforeLoad: ({ context, search }) => {
+    if (context.auth.isAuthenticated) {
+      throw redirect({
+        to: getSafeRedirectTarget(search.redirect),
+        replace: true,
+      });
+    }
+  },
+});
