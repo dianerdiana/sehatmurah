893d38f7461d4415bb0b868e608dd860534a1003
dianerdiana <dianerdiana.de@gmail.com>
2026-05-27 13:40:52 +0700

    feat: add registration page with layout and form integration

---
+++ frontend/src/routes/_layout-blank/auth.register.tsx
@@ -0,0 +1,37 @@
+import { createFileRoute, Link } from '@tanstack/react-router';
+
+import { themeConfig } from '@/configs/theme-config';
+
+import { FormRegister } from '../../modules/auth/components/form-register';
+
+export const Route = createFileRoute('/_layout-blank/auth/register')({
+  component: RegisterPage,
+});
+
+function RegisterPage() {
+  return (
+    <div className='grid min-h-svh lg:grid-cols-2'>
+      <div className='flex flex-col gap-4 p-6 md:p-10'>
+        <div className='flex justify-center gap-2 md:justify-start'>
+          <Link to='/' className='flex items-center gap-2 font-medium'>
+            <div className='flex w-60 items-center justify-center rounded-md text-primary-foreground'>
+              <img src={themeConfig.app.logoBrandName} alt='logo-brand' className='w-full h-auto' />
+            </div>
+          </Link>
+        </div>
+        <div className='flex flex-1 pt-10 justify-center'>
+          <div className='w-full max-w-xs'>
+            <FormRegister />
+          </div>
+        </div>
+      </div>
+      <div className='hidden bg-primary-light lg:block'>
+        <div className='w-full h-full flex flex-wrap items-center justify-center'>
+          <div className='flex flex-wrap justify-center'>
+            <img src='/assets/image/login-image.png' alt='Image' className='inset-0 h-full w-full object-cover' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
