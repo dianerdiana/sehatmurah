e6253163363c4dea4d55c5f40ddc3363202675ba
dianerdiana <dianerdiana.de@gmail.com>
2026-05-29 14:56:39 +0700

    feat: add create and update user mutation options to userMutationOptions

--- frontend/src/modules/users/user.mutation.ts
+++ frontend/src/modules/users/user.mutation.ts
@@ -2,8 +2,21 @@ import { mutationOptions } from '@tanstack/react-query';
 
 import { userApi } from './user.api';
 import { userKeys } from './user.key';
+import type { CreateUserDto, UpdateUserDto } from './user.schema';
 
 export const userMutationOptions = {
+  create: () =>
+    mutationOptions({
+      mutationKey: userKeys.create(),
+      mutationFn: (payload: CreateUserDto) => userApi.create(payload),
+    }),
+
+  update: (id: string) =>
+    mutationOptions({
+      mutationKey: userKeys.update(id),
+      mutationFn: (payload: UpdateUserDto) => userApi.update(id, payload),
+    }),
+
   delete: (id: string) =>
     mutationOptions({
       mutationKey: userKeys.delete(id),
