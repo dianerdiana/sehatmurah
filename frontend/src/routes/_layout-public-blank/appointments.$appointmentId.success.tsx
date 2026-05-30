32f57b2896dee429644f66feb49614199980508d
dianerdiana <dianerdiana.de@gmail.com>
2026-05-30 16:33:21 +0700

    feat: adjust layout structure in BookingAppointmentSuccess component for improved styling

--- frontend/src/routes/_layout-public-blank/appointments.$appointmentId.success.tsx
+++ frontend/src/routes/_layout-public-blank/appointments.$appointmentId.success.tsx
@@ -23,8 +23,8 @@ function BookingAppointmentSuccess() {
   }
 
   return (
-    <div className='flex flex-col justify-center gap-3 min-h-screen px-4'>
-      <section id='ContainerSuccess' className='w-full rounded-3xl bg-background'>
+    <div className='px-4 py-8 flex items-center'>
+      <section id='ContainerSuccess' className='w-full rounded-3xl py-8 bg-background'>
         <header className='mb-8 w-full px-4'>
           <img src='/assets/icons/succespage-booking-succes.svg' alt='Icon' className='mx-auto mb-6' />
           <h1 id='success-heading' className='text-center text-2xl font-extrabold leading-[30.24px]'>
@@ -34,7 +34,7 @@ function BookingAppointmentSuccess() {
           </h1>
         </header>
         <div id='BookingId' className='w-full px-4'>
-          <section className='mb-8 h-37 w-full rounded-3xl bg-[linear-gradient(100.99deg,#277B53_0%,#277B7B_100%)] pb-6'>
+          <section className='h-37 w-full rounded-3xl bg-[linear-gradient(100.99deg,#277B53_0%,#277B7B_100%)] pb-6'>
             <div className='w-full rounded-3xl bg-[linear-gradient(100.99deg,#277B53_0%,#277B7B_100%)] p-0.5'>
               <div className='mb-4 flex h-14 w-full items-center gap-x-1 rounded-3xl bg-background p-4'>
                 <img src='/assets/icons/succespage-booking-id.svg' alt='Icon' loading='lazy' />
@@ -48,21 +48,23 @@ function BookingAppointmentSuccess() {
           </section>
         </div>
       </section>
-      <footer className='flex flex-col gap-3'>
-        <Button className='text-base rounded-full leading-[20.16px] font-semibold py-7' asChild>
-          <Link id='CtaViewBooking' to='/future-appointment-pending'>
-            View My Booking
-          </Link>
-        </Button>
-        <Button
-          variant='ghost'
-          className='text-base text-primary hover:text-primary bg-primary-light/30 rounded-full leading-[20.16px] font-semibold py-7'
-          asChild
-        >
-          <Link id='CtaViewBooking' to='/appointments'>
-            Make Another Appointment
-          </Link>
-        </Button>
+      <footer className='fixed bottom-0 left-0 right-0 mx-auto max-w-170 p-4 bg-white rounded-3xl'>
+        <div className='flex flex-col gap-3'>
+          <Button className='text-base rounded-full leading-[20.16px] font-semibold py-7' asChild>
+            <Link id='CtaViewBooking' to='/future-appointment-pending'>
+              View My Booking
+            </Link>
+          </Button>
+          <Button
+            variant='ghost'
+            className='text-base text-primary hover:text-primary bg-primary-light/30 rounded-full leading-[20.16px] font-semibold py-7'
+            asChild
+          >
+            <Link id='CtaViewBooking' to='/appointments'>
+              Make Another Appointment
+            </Link>
+          </Button>
+        </div>
       </footer>
     </div>
   );
