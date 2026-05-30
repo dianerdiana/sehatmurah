0d29c9e223507034b9927e9e4c13b992a2c71ed9
dianerdiana <dianerdiana.de@gmail.com>
2026-05-30 09:42:08 +0700

    feat: refactor mapFilesToBody middleware to streamline file handling and improve readability

--- backend/src/middlewares/map-file-to-body.middleware.ts
+++ backend/src/middlewares/map-file-to-body.middleware.ts
@@ -3,15 +3,34 @@ import type { NextFunction, Request, Response } from 'express';
 export const mapFilesToBody = async (req: Request, _res: Response, next: NextFunction) => {
   if (req.files && typeof req.files === 'object' && !Array.isArray(req.files)) {
     const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+    const uploadedFiles: Record<string, string[]> = {};
+
+    if (files['icon']?.length) {
+      uploadedFiles.icon = files['icon'].map((file) => `icons/${file.filename}`);
+    }
+
+    if (files['image']?.length) {
+      uploadedFiles.image = files['image'].map((file) => `images/${file.filename}`);
+    }
+
+    if (files['profilePhoto']?.length) {
+      uploadedFiles.profilePhoto = files['profilePhoto'].map((file) => `doctors/${file.filename}`);
+    }
+
+    req.uploadedFiles = uploadedFiles;
 
     // Jika ada file icon, simpan dengan format: icons/nama-file.png
-    if (files['icon'] && files['icon'][0]) {
-      req.body.icon = `icons/${files['icon'][0].filename}`;
+    if (uploadedFiles.icon?.[0]) {
+      req.body.icon = uploadedFiles.icon[0];
     }
 
     // Jika ada file image, simpan dengan format: images/nama-file.png
-    if (files['image'] && files['image'][0]) {
-      req.body.image = `images/${files['image'][0].filename}`;
+    if (uploadedFiles.image?.[0]) {
+      req.body.image = uploadedFiles.image[0];
+    }
+
+    if (uploadedFiles.profilePhoto?.[0]) {
+      req.body.profilePhoto = uploadedFiles.profilePhoto[0];
     }
   }
 
