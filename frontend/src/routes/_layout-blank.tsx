69152beb882d4c3a336aa17aaa9ec329cbf51393
dianerdiana <dianerdiana.de@gmail.com>
2026-05-31 21:09:12 +0700

    fix: update theme hook usage in BlankLayout and adjust background color in LoginPage

--- frontend/src/routes/_layout-blank.tsx
+++ frontend/src/routes/_layout-blank.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router';
 
+import { useTheme } from '@/utils/hooks/use-theme';
+
 export const Route = createFileRoute('/_layout-blank')({
   component: BlankLayout,
 });
 
 function BlankLayout() {
+  const { setTheme } = useTheme();
+
+  setTheme('light');
+
   return <Outlet />;
 }
