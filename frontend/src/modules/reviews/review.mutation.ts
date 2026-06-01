b2241dc54136ee7c90d70750d3d3b117a45a6164
dianerdiana <dianerdiana.de@gmail.com>
2026-06-01 13:36:51 +0700

    feat: add update function to reviewMutationOptions for review updates

--- frontend/src/modules/reviews/review.mutation.ts
+++ frontend/src/modules/reviews/review.mutation.ts
@@ -2,13 +2,19 @@ import { mutationOptions } from '@tanstack/react-query';
 
 import { reviewApi } from './review.api';
 import { reviewKeys } from './review.key';
-import type { CreateReviewDto } from './review.schema';
+import type { CreateReviewDto, UpdateReviewDto } from './review.schema';
 
 export const reviewMutationOptions = {
   create: () =>
     mutationOptions({
       mutationKey: reviewKeys.create(),
       mutationFn: (payload: CreateReviewDto) => reviewApi.create(payload),
+    }),
+
+  update: (id: string) =>
+    mutationOptions({
+      mutationKey: reviewKeys.update(id),
+      mutationFn: (payload: UpdateReviewDto) => reviewApi.update(id, payload),
     }),
 
   delete: (id: string) =>
